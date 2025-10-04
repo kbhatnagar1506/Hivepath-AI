4206e892d81aca6356feebf6ae8e372bc72f0ce3
KRISHNA BHATNAGAR <krishna@vfcloans.com>
2025-10-04 14:19:24 -0400

    Fix uvicorn command in simple_app.py

--- swarmaura/simple_app.py
+++ swarmaura/simple_app.py
@@ -43,4 +43,4 @@ def api_status():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("simple_app:app", host="0.0.0.0", port=port)
